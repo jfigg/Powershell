1a146882694cd0cddcbc4ba05f25db07bcbb2c98
jfigg <figgjacob@gmail.com>
2017-06-01 14:37:21 -0400

    xml

---
+++ CurrentUpdates.ps1
@@ -0,0 +1,24 @@
+﻿ 
+
+$updates = @()
+$server = "localhost"
+
+
+    write-host -fore DarkGreen "Checking $server"
+    Try {
+        $osname = (Get-WmiObject -computername "$server" Win32_OperatingSystem).Name
+        $osname = $osname.Substring(0, $osname.IndexOf('|'))
+        $current = (get-hotfix -ComputerName "$server" -ErrorAction stop | sort installedon)[-1] 
+        $current | Add-Member -type NoteProperty -Name 'OSName' -Value "$osname"
+        $updates += $current
+        write-host -fore Green "$server Checked"
+    }
+    Catch {
+        $updates += "$server failed to retrieve"
+        write-host -fore Red "$server failed to retrieve"
+        continue
+    }
+
+
+
+$updates | format-table PSComputerName,Description,HotFixID,InstalledOn,OSName 
---
+++ xmlread.ps1
@@ -0,0 +1,5 @@
+﻿ [xml]$XmlDocument = Get-Content -Path C:\users\nhti\desktop\test.xml
+#$XmlDocument | select ChildNodes | % { write-host $_.applications }
+
+#$XmlDocument.postdeploydata.regkeys.delete | % { write-host $_ }
+$XmlDocument.postdeploydata.regkeys.delete | % { reg delete "$_" }
